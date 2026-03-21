43e115deff7cc8121223dd6a31c0786875a94c7f
Lamprro <140699726+Lamprro@users.noreply.github.com>
2026-03-21 10:20:25 +0700

    start the project

---
+++ src/main/java/com/example/Sapo_Clone/SapoCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sapo_Clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapoCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapoCloneApplication.class, args);
+	}
+
+}
